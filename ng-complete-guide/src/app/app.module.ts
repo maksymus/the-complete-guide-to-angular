4fc9028e64032dbb2483d5d2023a194f17569bc0
maksym <maksymus@gmail.com>
2017-12-10 11:50:53 -0500

    NgRx Redux

--- ng-complete-guide/src/app/app.module.ts
+++ ng-complete-guide/src/app/app.module.ts
@@ -2,6 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
+import { StoreModule } from '@ngrx/store';
+
 import {AppComponent} from './app.component';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -9,6 +11,7 @@ import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
 import {CoreModule} from './core/core.module';
+import {shoppingListReducer} from "./shopping-list/store/shopping-list.reducers";
 
 @NgModule({
   declarations: [
@@ -21,7 +24,8 @@ import {CoreModule} from './core/core.module';
     CoreModule,
     ShoppingListModule,
     AuthModule,
-    SharedModule
+    SharedModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer})
   ],
   bootstrap: [AppComponent]
 })
